9327c90a8987393278cc07aa15dcfa118ff5d93c
Artem Kobrin <kobrin.artem@gmail.com>
2015-09-28 23:12:04 +0300

    compiled  wsgi with Python 2.7.8

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ override['python']['binary'] = "#{node['python']['prefix_dir']}/bin/python"
 override['python']['url'] = 'http://www.python.org/ftp/python'
 override['python']['version'] = '2.7.8'
 override['python']['checksum'] = '74d70b914da4487aa1d97222b29e9554d042f825f26cb2b93abd20fdda56b557'
-override['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
+override['python']['configure_options'] = %W{--enable-shared --prefix=#{python['prefix_dir']}}
 override['python']['make_options'] = %W{install}
 
 override['python']['pip_location'] = "#{node['python']['prefix_dir']}/bin/pip"
--- recipes/wsgi.rb
+++ recipes/wsgi.rb
@@ -4,4 +4,22 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package 'mod_wsgi'
+package 'httpd-devel' do
+  action :install
+end
+
+python_virtualenv "/opt/python_virtualenv" do
+  interpreter "/opt/python/bin/python2.7"
+  action :create
+end
+
+ENV['CPPFLAGS'] = '-I/opt/python/include/python2.7'
+
+link '/usr/lib/libpython2.7.a' do
+  to '/opt/python/lib/libpython2.7.so'
+  link_type :symbolic
+end
+
+python_pip "mod_wsgi" do
+  virtualenv "/opt/python_virtualenv"
+end
