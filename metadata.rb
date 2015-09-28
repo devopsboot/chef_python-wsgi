0828dfd67f022994faf3cceec6b039e77fd15253
Artem Kobrin <kobrin.artem@gmail.com>
2015-09-28 23:29:09 +0300

    v0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'kobrin.artem@gmail.com'
 license 'all_rights'
 description 'Installs/Configures Python and mod_wsgi'
 long_description 'Installs/Configures python-wsgi'
-version '0.1.0'
+version '0.1.1'
 
 depends 'python', '~> 1.4.6'
